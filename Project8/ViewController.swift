f3d17b3417e2b6c2a960a82504d6be990ef6ea18
wkembel1563 <wkembel@hotmail.com>
2024-03-27 22:50:50 -0500

    Initial Commit

---
+++ Project8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project8
+//
+//  Created by Will Kembel on 3/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
